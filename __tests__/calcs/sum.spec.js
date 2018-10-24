50c14ca05a77b1d78038782d5bdcfd439d88b861
Samuel Silva <samuelsilvawb@gmail.com>
2018-10-24 06:18:53 +0100

    test: cover sum module with tests
    - Added more tests to module.

--- __tests__/calcs/sum.spec.js
+++ __tests__/calcs/sum.spec.js
@@ -5,9 +5,21 @@ describe('# SUM Module', () => {
 		expect(typeof sum).toBe('function');
 	});
 
-	it('should return 5 when pass 3 and 2 as argument', () => {
+	it('should return 5 when pass ( 3, 2 ) as argument', () => {
 		const calc = sum(3, 2);
 
 		expect(calc).toBe(5);
+	});
+
+	it('should return 10 when pass( 4, 4, 2 ) as argument', () => {
+		const calc = sum(4, 4, 2);
+
+		expect(calc).toBe(10);
+	});
+
+	it('should not break if I not pass nothing', () => {
+		const calc = sum();
+
+		expect(calc).toBe(0);
 	});
 });
