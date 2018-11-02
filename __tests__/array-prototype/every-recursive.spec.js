b82642c453cdbaf56d8de29483a67f1ea744b680
Samuel Silva <samuelsilvawb@gmail.com>
2018-11-02 17:58:36 +0100

    feat: add recursive every function from Array.prototype
    - Created function with the same logic of every from array.prototype using recursivity.
    - Copied test from old filter function

---
+++ __tests__/array-prototype/every-recursive.spec.js
@@ -0,0 +1,33 @@
+const every = require('../../modules/array-prototype/every-recursive');
+
+describe('# ARRAY PROTOTYPE Module - EVERY RECURSIVE', () => {
+	it('should return a function', () => {
+		expect(typeof every).toBe('function');
+	});
+
+	it('should return `true` when test if all items are bigger than zero', () => {
+		const arr = [1, 2, 3];
+		const filterBiggerThanZero = (item) => {
+			return item > 0;
+		};
+
+		expect(every(arr, filterBiggerThanZero)).toBe(true);
+	});
+
+	it('should return `false` when test if all items are bigger than zero', () => {
+		const arr = [0, 2, 3];
+		const filterBiggerThanZero = (item) => {
+			return item > 0;
+		};
+
+		expect(every(arr, filterBiggerThanZero)).toBe(false);
+	});
+
+	it('should return `true` if no argument is passed', () => {
+		expect(every()).toBe(true);
+	});
+
+	it('should return `true` if only array is passed', () => {
+		expect(every([])).toBe(true);
+	});
+});
---
+++ modules/array-prototype/every-recursive.js
@@ -0,0 +1,14 @@
+function every(arr, callback) {
+	return (function everyInternal(arrayInternal, counter) {
+		if (!arr || !callback || arrayInternal.length === 0) {
+			return true;
+		}
+
+		const head = arrayInternal.slice(0, 1);
+		const tail = arrayInternal.slice(1);
+
+		return !callback(head, counter, arr) ? false : everyInternal(tail, counter + 1);
+	}(arr, 0));
+}
+
+module.exports = every;
