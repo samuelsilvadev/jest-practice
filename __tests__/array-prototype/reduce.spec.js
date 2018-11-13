02237e6c10b939b1f29fe87942b96291f15f52bf
Samuel Silva <samuelsilvawb@gmail.com>
2018-11-13 07:50:19 +0100

    test: cover reduce function from Array.prototype
    - Added tests to reduce function.

---
+++ __tests__/array-prototype/reduce.spec.js
@@ -0,0 +1,46 @@
+const reduce = require('../../modules/array-prototype/reduce');
+
+describe('# ARRAY PROTOTYPE Module - REDUCE', () => {
+	it('should return a function', () => {
+		expect(typeof reduce).toBe('function');
+	});
+
+	it('should return 3 when pass reduce([1, 2], (acc, item) => acc + item, 0)', () => {
+		const result = reduce([1, 2], (acc, item) => acc + item, 0);
+
+		expect(result).toBe(3);
+	});
+
+	it('should return 3 when pass reduce([1, 2], (acc, item) => acc + item)', () => {
+		const result = reduce([1, 2], (acc, item) => acc + item);
+
+		expect(result).toBe(3);
+	});
+
+	it('should return 0 when pass reduce([], (acc, item) => acc + item, 0)', () => {
+		const result = reduce([], (acc, item) => acc + item, 0);
+
+		expect(result).toBe(0);
+	});
+
+	it('should return { a0: 1 } when pass reduce([1], (acc, item, index) => { acc[a + index] = index; return acc; }, {})', () => {
+		const result = reduce([1], (acc, item, index) => {
+			acc[`a${index}`] = item;
+			return acc;
+		}, {});
+
+		expect(result).toEqual({ a0: 1 });
+	});
+
+	it('should return 12 when pass reduce([1, 2, 3], (acc, item, index, arr) => acc + item + arr[index], 0)', () => {
+		const result = reduce([1, 2, 3], (acc, item, index, arr) => {
+			return acc + item + arr[index];
+		}, 0);
+
+		expect(result).toEqual(12);
+	});
+
+	it('should throw an TypeError when pass reduce([], (acc, item) => acc + item)', () => {
+		expect(() => reduce([], (acc, item) => acc + item)).toThrowError('Reduce of empty array with no initial value');
+	});
+});
