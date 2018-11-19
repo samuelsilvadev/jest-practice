abef6cc240a0f1686aec7b1ba988e8259138b047
Samuel Silva <samuelsilvawb@gmail.com>
2018-11-19 07:58:41 +0000

    test: cover find function from Array.prototype
    - Added tests to find function.

---
+++ __tests__/array-prototype/find.spec.js
@@ -0,0 +1,39 @@
+const find = require('./../../modules/array-prototype/find.js');
+
+describe('# ARRAY PROTOTYPE Module - FIND', () => {
+	it('should return a function', () => {
+		expect(typeof find).toBe('function');
+	});
+
+	it('should return 1 when pass find([1, 2], (i) => i < 2))', () => {
+		const arr = [1, 2];
+		const result = find(arr, (i) => i < 2);
+
+		expect(result).toBe(1);
+	});
+
+	it('should return 2 when pass find([4, 2, 5, 7], (i) => i < 3))', () => {
+		const arr = [4, 2, 5, 7];
+		const result = find(arr, (i) => i < 3);
+
+		expect(result).toBe(2);
+	});
+
+	it('should return `undefined` when pass find([1, 2], (i) => i > 10))', () => {
+		const arr = [1, 2];
+		const result = find(arr, (i) => i > 10);
+
+		expect(result).toBeUndefined();
+	});
+
+	it('should return `undefined` when pass find([], (i) => i > 10))', () => {
+		const arr = [];
+		const result = find(arr, (i) => i > 10);
+
+		expect(result).toBeUndefined();
+	});
+
+	it('should return `undefined` when pass find()', () => {
+		expect(find()).toBeUndefined();
+	});
+});
