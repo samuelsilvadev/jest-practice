72c453698d27edd5d34161c56f70aa898274c175
Samuel SIlva <samuelsilvawb@gmail.com>
2019-01-29 09:10:13 +0100

    refactor: improve typo on factoralize test
    - Improved variables on tests.

--- __tests__/algorithms/factoralize.spec.js
+++ __tests__/algorithms/factoralize.spec.js
@@ -6,38 +6,44 @@ describe('Factoralize a number', () => {
 	});
 
 	it('should return a number', () => {
-		const expectedResult = factoralize(1);
+		const result = factoralize(1);
+		const expectedResult = 'number';
 
-		expect(typeof expectedResult).toBe('number');
+		expect(typeof result).toBe(expectedResult);
 	});
 
-	it('should return `120` when `5` was passed', () => {
-		const expectedResult = factoralize(5);
+	it('should return `120` when `5` is pass', () => {
+		const result = factoralize(5);
+		const expectedResult = 120;
 
-		expect(expectedResult).toBe(120);
+		expect(result).toBe(expectedResult);
 	});
 
-	it('should return `2432902008176640000` when `20` was passed', () => {
-		const expectedResult = factoralize(20);
+	it('should return `2432902008176640000` when `20` is pass', () => {
+		const result = factoralize(20);
+		const expectedResult = 2432902008176640000;
 
-		expect(expectedResult).toBe(2432902008176640000);
+		expect(result).toBe(expectedResult);
 	});
 
-	it('should return `1` when `0` was passed', () => {
-		const expectedResult = factoralize(0);
+	it('should return `1` when `0` is pass', () => {
+		const result = factoralize(0);
+		const expectedResult = 1;
 
-		expect(expectedResult).toBe(1);
+		expect(result).toBe(expectedResult);
 	});
 
-	it('should throw an error if a argument different of number was passed', () => {
-		const expectedResult = () => factoralize('0');
+	it('should throw an error if a argument different of number is pass', () => {
+		const result = () => factoralize('0');
+		const expectedResult = 'Argument needs to be a number';
 
-		expect(expectedResult).toThrowError('Argument needs to be a number');
+		expect(result).toThrowError(expectedResult);
 	});
 
-	it('should throw an error if an undefined argument was passed', () => {
-		const expectedResult = () => factoralize();
+	it('should throw an error if an undefined argument is pass', () => {
+		const result = () => factoralize();
+		const expectedResult = 'Argument should not be empty';
 
-		expect(expectedResult).toThrowError('Argument should not be empty');
+		expect(result).toThrowError(expectedResult);
 	});
 });
