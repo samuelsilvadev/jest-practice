fbf62be3585079ad3903c36a5215adce70bc26dd
Samuel Silva <samuelsilvawb@gmail.com>
2020-01-26 21:54:00 +0100

    feat: Add sumZero logic

---
+++ modules/data-structures/pointers/sumZero.js
@@ -0,0 +1,24 @@
+function sumZero(array) {
+	let head = 0;
+	let tail = array.length - 1;
+
+	while (head < tail) {
+		if (array[head] === 0 || array[tail] === 0) {
+			break;
+		}
+
+		const delta = array[head] + array[tail];
+
+		if (delta < 0) {
+			tail--;
+		} else if (delta > 0) {
+			head++;
+		} else {
+			return [array[head], array[tail]];
+		}
+	}
+
+	return [];
+}
+
+module.exports = sumZero;
